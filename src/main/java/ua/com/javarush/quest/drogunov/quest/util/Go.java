fc5779120ba346a0d5986ea26a5b954702258b1c
Sergey <drogunov-s@yandex.ru>
2022-09-25 15:48:53 +0300

    commit

--- src/main/java/ua/com/javarush/quest/drogunov/quest/util/Go.java
+++ src/main/java/ua/com/javarush/quest/drogunov/quest/util/Go.java
@@ -1,4 +1,24 @@
 package ua.com.javarush.quest.drogunov.quest.util;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class Go {
+    public final String HOME = "";
+    public final String ROOT = "/";
+    public final String ALL = "/*";
+
+    public final String SIGNUP = "/signup";
+    public final String LOGIN = "/login";
+    public final String LOGOUT = "/logout";
+    public final String PROFILE = "/profile";
+    public final String USERS = "/users";
+    public final String EDIT_USER = "/edit-user";
+
+    public final String CREATE = "/create";
+    public final String QUEST = "/quest";
+    public final String QUESTS = "/quests";
+    public final String START = "/start";
+    public final String GAME = "/game";
+
 }
